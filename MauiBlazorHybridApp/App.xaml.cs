9477a1a76c893f583aa69b53d52365021042d51e
Erick Kurniawan <erick.kurniawan@gmail.com>
2025-11-13 18:16:38 +0700

    Add project files.

---
+++ MauiBlazorHybridApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiBlazorHybridApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "MauiBlazorHybridApp" };
+        }
+    }
+}
